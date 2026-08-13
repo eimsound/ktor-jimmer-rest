a4a44bd012384401b8a0e8f4ac68d22909874a25
snarrow <maqueyuxue@outlook.com>
2026-08-13 18:55:40 +0800

    chore(release): 0.1.0 发布配置（JitPack 兼容）

--- ktor-jimmer-rest/settings.gradle.kts
+++ ktor-jimmer-rest/settings.gradle.kts
@@ -2,8 +2,13 @@ rootProject.name = "ktor-jimmer-rest"
 
 dependencyResolutionManagement{
     repositories {
-        maven("https://mirrors.tencent.com/nexus/repository/maven-public/")
-        mavenCentral()
+        if (System.getenv("JITPACK") == "true") {
+            // JitPack 构建机访问国内镜像不稳定，仅使用 Maven Central 防止超时
+            mavenCentral()
+        } else {
+            maven("https://mirrors.tencent.com/nexus/repository/maven-public/")
+            mavenCentral()
+        }
     }
 }
 include("ktor-jimmer-rest-route")
@@ -11,4 +16,3 @@ include("ktor-jimmer-rest-provider")
 include("ktor-jimmer-rest-util")
 include("ktor-jimmer-rest-validator")
 include("ktor-jimmer-rest-config")
-
